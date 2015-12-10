149e0060477c8ce82db9586c7b7445098a807eda
sltiedeman <sltiedeman@gmail.com>
2015-12-10 13:06:35 -0500

    Minor timing tweak of timeout function

--- js/scripts.js
+++ js/scripts.js
@@ -139,7 +139,7 @@ $(document).ready(function(){
 			var paddingAdjust = parseInt((captionHeight - captionContentHeight)/2);
 			$(captionContent).css('padding-top', paddingAdjust + 'px');
 		}
-	},2000);
+	},1000);
 
 	//Calculates page height so that progress bars do not animate 
 	//until user reaches the right spot on the page
